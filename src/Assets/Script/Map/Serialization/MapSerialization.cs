9cb443f8ccec9f2a43cad04c62f58190b0b2b5fb
Aer_Vento. <wz407021500@163.com>
2023-09-29 20:51:36 +0800

    fix: remove useless script

--- src/Assets/Script/Map/Serialization/MapSerialization.cs
+++
@@ -1,9 +0,0 @@
-using Newtonsoft.Json;
-
-namespace GrowTopia.Serialization{
-    class MapSerializationSettings : JsonSerializerSettings{
-        public MapSerializationSettings(){
-            
-        }
-    }
-}
